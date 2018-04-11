9f83971f3e9ec13df4b52359568848365ebcb2f3
Alabe Duarte <alabeduarte@gmail.com>
2018-04-11 22:45:08 +1000

    add functions exercises from chapter 5

---
+++ README.md
@@ -0,0 +1,3 @@
+# examples-from-programming-elixir
+
+Examples from [Programming Elixir book](https://pragprog.com/book/elixir16/programming-elixir-1-6).
---
+++ chapter-5/functions-1.exs
@@ -0,0 +1,20 @@
+#Go into iex. Create and run the functions that do the following:
+#– list_concat.([:a, :b], [:c, :d]) #=> [:a, :b, :c, :d]
+#– sum.(1, 2, 3) #=> 6
+#– pair_tuple_to_list.( { 1234, 5678 } ) #=> [ 1234, 5678 ]
+
+list_concat = fn(list_a, list_b) ->
+  list_a ++ list_b
+end
+
+sum = fn(a, b, c) ->
+  Enum.reduce([a, b, c], fn(n, acc) -> n + acc end)
+end
+
+pair_tuple_to_list = fn { a, b } ->
+  [a, b]
+end
+
+IO.inspect(list_concat.([:a, :b], [:c, :d]))
+IO.inspect(sum.(1, 2, 3))
+IO.inspect(pair_tuple_to_list.( { 1234, 5678 }))
