653822c7df1ec0967503494b16a22dd6b3c740c2
Alabe Duarte <alabeduarte@gmail.com>
2018-04-15 23:32:06 +1000

    add modules-and-functions-4 from chapter 6

---
+++ chapter-6/modules_and_functions-4.exs
@@ -0,0 +1,12 @@
+# ➤ Exercise: ModulesAndFunctions-4
+# Implement and run a function sum(n) that uses recursion to calculate the sum
+# of the integers from 1 to n. You’ll need to write this function inside a
+# module in a separate file. Then load up iex, compile that file, and try your
+# function.
+
+defmodule Sum do
+  def of(1), do: 1
+  def of(n), do: n + of(n-1)
+end
+
+IO.inspect(Sum.of(5))
