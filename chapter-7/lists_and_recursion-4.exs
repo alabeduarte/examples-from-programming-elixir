57d4d54df8a0019024cd233246e80eba44817061
Alabe Duarte <alabeduarte@gmail.com>
2018-04-27 10:51:20 +1000

    add span function from chapter-7

---
+++ chapter-7/lists_and_recursion-4.exs
@@ -0,0 +1,11 @@
+# ➤ Exercise: ListsAndRecursion-4
+# Write a function MyList.span(from, to) that returns a list of the numbers
+# from `from` up to `to`.
+
+defmodule MyList do
+  def span(to, to), do: [to]
+  def span(from, to) when from > to, do: span(to, from)
+  def span(from, to), do: [from | span(from + 1, to)]
+end
+
+IO.inspect MyList.span(10, 1)
