401294779bfda1b39f47b93cb622b189970da406
Alabe Duarte <alabeduarte@gmail.com>
2018-12-19 14:43:58 +1100

    chapter-13: Fetch github issues upon request

--- chapter-13/issues/lib/issues/cli.ex
+++ chapter-13/issues/lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -22,15 +24,48 @@ defmodule Issues.CLI do
     |> args_to_internal_representation()
   end
 
-  def args_to_internal_representation([user, project, count]) do
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
+  end
+
+  defp args_to_internal_representation([user, project, count]) do
     {user, project, String.to_integer(count)}
   end
 
-  def args_to_internal_representation([user, project]) do
+  defp args_to_internal_representation([user, project]) do
     {user, project, @default_count}
   end
 
-  def args_to_internal_representation(_) do
+  defp args_to_internal_representation(_) do
     :help
   end
 end
