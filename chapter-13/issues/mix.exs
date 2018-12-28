15af07fe5d4daec5418b3bf5d24ecd6193b3d888
Alabe Duarte <alabeduarte@gmail.com>
2018-12-28 15:48:52 +1100

    chapter-14: collect test coverage

--- chapter-13/issues/mix.exs
+++ chapter-13/issues/mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       source_url: "https://github.com/alabeduarte/examples-from-programming-elixir",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -25,7 +32,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.4"},
       {:poison, "~> 4.0.1"},
       {:ex_doc, "~> 0.19.2"},
-      {:earmark, "~> 1.3"}
+      {:earmark, "~> 1.3", override: true},
+      {:excoveralls, "~> 0.10.3"}
     ]
   end
 
