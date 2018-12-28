8f90521bedfa8e976f26ab198eab0d5f21e0561b
Alabe Duarte <alabeduarte@gmail.com>
2018-12-28 15:44:14 +1100

    chapter-14: add doc tests

--- chapter-13/issues/lib/issues/table_row.ex
+++ chapter-13/issues/lib/issues/table_row.ex
@@ -1,4 +1,12 @@
 defmodule Issues.TableRow do
+  @doc """
+  Return a binary (string) version of our parameter.
+  ## Examples
+  iex> Issues.TableRow.printable("a")
+  "a"
+  iex> Issues.TableRow.printable(99)
+  "99"
+  """
   def printable(str) when is_binary(str), do: str
   def printable(str), do: to_string(str)
 end
