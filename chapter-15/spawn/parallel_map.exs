0582db28deaf647bcf11f11dc88103cd08f56d62
Alabe Duarte <alabeduarte@gmail.com>
2018-12-31 10:50:35 +1100

    chapter-15: add example with parallel map

---
+++ chapter-15/spawn/parallel_map.exs
@@ -0,0 +1,19 @@
+defmodule Parallel do
+  @doc """
+  iex> Parallel.pmap 1..10, &(&1 * &1)
+  [1,4,9,16,25,36,49,64,81,100]
+  """
+  def pmap(collection, fun) do
+    me = self()
+
+    collection
+    |> Enum.map(fn elem ->
+      spawn_link(fn -> send(me, {self(), fun.(elem)}) end)
+    end)
+    |> Enum.map(fn pid ->
+      receive do
+        {^pid, result} -> result
+      end
+    end)
+  end
+end
