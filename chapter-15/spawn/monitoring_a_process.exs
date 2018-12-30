6506926e171b088beb99d3b2fe55b11e0e6a8b6d
Alabe Duarte <alabeduarte@gmail.com>
2018-12-31 10:44:11 +1100

    chapter-15: add example with spawn_monitor

---
+++ chapter-15/spawn/monitoring_a_process.exs
@@ -0,0 +1,23 @@
+defmodule MonitoringAProcess.Monitor do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    res = spawn_monitor(MonitoringAProcess.Monitor, :sad_function, [])
+    IO.puts(inspect(res))
+
+    receive do
+      msg ->
+        IO.puts("MESSAGE RECEIVED: #{inspect(msg)}")
+    after
+      1000 ->
+        IO.puts("Nothing happened as far as I am concerned")
+    end
+  end
+end
+
+MonitoringAProcess.Monitor.run()
